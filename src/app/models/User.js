a2339f5501967e4f8f44dfa06f1cc3b2b41bb183
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2020-02-27 19:50:25 -0300

    incializando repositorio e adicionando sessioncontroller

---
+++ src/app/models/User.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcryptjs";
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        is_admin: Sequelize.BOOLEAN
+      },
+      {
+        sequelize
+      }
+    );
+  }
+
+  async checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+export default User;
