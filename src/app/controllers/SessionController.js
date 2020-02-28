ef4b8a89b931d515ff8c1070e262e993675b192a
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2020-02-27 22:08:10 -0300

    adicionando validação de dados

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,8 +3,23 @@ import authConfig from '../../config/AuthConfig';
 
 import User from '../models/User';
 
+const yup = require('yup');
+
 class SessionController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup
+        .string()
+        .email()
+        .required(),
+      password: yup
+        .string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: 'Validator error' });
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
